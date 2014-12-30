12d34548dc46a7e100c203ba142715e9013d79f4
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2014-12-30 15:37:55 +0100

    Change order of lo rule

--- setup_firewall.sh
+++ setup_firewall.sh
@@ -5,6 +5,7 @@ iptables -A INPUT -p tcp -m tcp -m multiport --dports 1337,53289 -j ACCEPT
 iptables -A INPUT -p udp -m udp -m multiport --dports 53290 -j ACCEPT
 iptables -A INPUT -m conntrack -j ACCEPT  --ctstate RELATED,ESTABLISHED
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -j DROP
 
 # This would only allow outgoing related traffic!
@@ -17,6 +18,3 @@ iptables -A OUTPUT -d 0.0.0.0/0 -j ACCEPT
 # That's ok
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -j DROP
-
-# Allow localhost
-iptables -A INPUT -i lo -j ACCEPT
